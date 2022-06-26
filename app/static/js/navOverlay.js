de5209ade9b8381cf0f10f257cd75b102cff5a99
mxfng <mxfng@Studio-Mac-mini.lan>
2022-06-25 23:26:21 -0700

    Super dope toggler button

--- app/static/js/navOverlay.js
+++ app/static/js/navOverlay.js
@@ -1,17 +1,6 @@
 // Toggle Icon
-function toggleNavIcon(icon) {
-    icon.classList.toggle("x-lg");
-}
-
-$(function() {
-  
-    $('#navbarMenu')
-        .on('shown.bs.collapse', function() {
-            $('#toggler-hamburger').addClass('hidden');
-            $('#toggler-close').removeClass('hidden');    
-        })
-        .on('hidden.bs.collapse', function() {
-            $('#toggler-hamburger').removeClass('hidden');
-            $('#toggler-close').addClass('hidden');        
-    });   
+$(document).ready(function(){
+	$('#navbar-icon').click(function(){
+		$(this).toggleClass('open');
+	});
 });
