1a201fa376b443e6095caa729083f4e0319d2356
mxfng <mxfng@Studio-Mac-mini.lan>
2022-06-25 21:11:20 -0700

    More updates

--- app/static/js/navOverlay.js
+++ app/static/js/navOverlay.js
@@ -2,3 +2,16 @@
 function toggleNavIcon(icon) {
     icon.classList.toggle("x-lg");
 }
+
+$(function() {
+  
+    $('#navbarMenu')
+        .on('shown.bs.collapse', function() {
+            $('#toggler-hamburger').addClass('hidden');
+            $('#toggler-close').removeClass('hidden');    
+        })
+        .on('hidden.bs.collapse', function() {
+            $('#toggler-hamburger').removeClass('hidden');
+            $('#toggler-close').addClass('hidden');        
+    });   
+});
