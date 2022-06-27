0184334e6cc03d26170b33afe83c41ad9e95ba5b
mxfng <mxfng@Studio-Mac-mini.lan>
2022-06-27 16:03:51 -0700

    Refined disc view

--- app/static/js/discBackgroundColor.js
+++
@@ -1,9 +0,0 @@
-// Transitions page color for disc view
-function discBackgroundColor(color) {
-    backgroundColor = color;
-    var headerSection = document.getElementsByClassName('header-section')[0];
-    document.body.classList.add('disc-background-fade');
-    headerSection.classList.add('disc-background-fade');
-    document.body.style.background = backgroundColor;
-    headerSection.style.background = backgroundColor;
-};
