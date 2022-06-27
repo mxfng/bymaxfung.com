ea2e08823f2990e469900e31042d0a6cded2febc
mxfng <mxfng@Studio-Mac-mini.lan>
2022-06-26 20:21:09 -0700

    Fixed resize toggler issue

--- app/static/js/mobileNavToggle.js
+++ app/static/js/mobileNavToggle.js
@@ -2,7 +2,14 @@
 var icon = document.getElementById('navbar-icon');
 var content = document.getElementsByClassName('content')[0];
 icon.addEventListener('click', function() {
-	document.body.classList.toggle('noscroll');
-	content.classList.toggle('hidden');
-	this.classList.toggle('open');
+	document.body.classList.toggle('noscroll');	// Lock scrolling
+	content.classList.toggle('hidden');			// Hide page content
+	this.classList.toggle('open');				// Animate the toggler icon
+});
+
+// Handle resize
+window.addEventListener('resize', function() {
+	if (window.innerWidth >= 992 && window.getComputedStyle(content).visibility == 'hidden') {
+		icon.click() // Close out the mobile navbar & associated locks
+	}
 });
