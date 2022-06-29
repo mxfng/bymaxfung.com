63761f13975ccfb5a6e387150b3d07add9002623
mxfng <mxfng@Studio-Mac-mini.lan>
2022-06-29 02:23:49 -0700

    Music updates

--- app/routes/music.py
+++ app/routes/music.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, url_for
 from datetime import datetime
+from os import path, listdir
 
 music = Blueprint("music", __name__)
 
@@ -9,7 +10,15 @@ def view_music():
 
 @music.get("/music/<string:title>")
 def display_music(title: str):
-    try:
-        return render_template(f'music/discography/{title}.html')
+    print(url_for('static', filename=f'img/galleries/{title}/*.jpeg'))
+    print(path.join(
+        music.root_path,
+        url_for('static',
+                filename=f'img/galleries/{title}')[1:]
+        )
+    )
+    return render_template(f'music/discography/{title}.html')
+    '''
     except:
         abort(404)
+        '''
