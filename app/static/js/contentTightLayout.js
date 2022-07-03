6ceae8a008993a120b21eec81c26d9fabb89879c
Max Fung <mxfng@Maxs-MacBook-Pro.local>
2022-07-03 09:48:23 -0400

    img cleanup, about css fix

--- app/static/js/contentTightLayout.js
+++ app/static/js/contentTightLayout.js
@@ -1,4 +1,7 @@
 // Applies the tight layout class to the content div
 // Used on templates inheriting the wider content div from base.html
 var content = document.getElementsByClassName('content')[0];
+console.log('hello world')
+console.log(content);
 content.classList.add('tight');
+content.style.maxWidth = '500px';
